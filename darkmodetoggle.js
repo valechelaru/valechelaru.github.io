7b7e5980ad2499d32e815a52a2265647b31d2e79
Valentin Chelaru <valechelaru@gmail.com>
2022-02-16 18:20:37 +0100

    Starting to fill up website with content. Header, preamble and summary filled

--- darkmodetoggle.js
+++ darkmodetoggle.js
@@ -1,5 +1,6 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+// How to check for dark mode in client browser: window.matchMedia('(prefers-color-scheme: dark)').matches
 if (sessionStorage.getItem("darkmode") === 'true') {
     document.documentElement.setAttribute('data-theme', 'dark');
     toggleSwitch.toggleAttribute("checked");
