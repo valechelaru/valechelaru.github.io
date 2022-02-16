60f4458c215fa28d76fadc4956c22cc52e06563a
Valentin Chelaru <valechelaru@gmail.com>
2022-02-16 02:01:45 +0100

    Little cleanup, added simple favicon and working on the color coding of the site

--- darkmodetoggle.js
+++ darkmodetoggle.js
@@ -1,11 +1,19 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+var darkmodestate = sessionStorage.getItem("darkmode")
+
+if (darkmodestate) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+}
+
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        sessionStorage.setItem("darkmode", true);
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        sessionStorage.setItem("darkmode", false);
     }
 }
 
